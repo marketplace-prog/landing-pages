17eebf92d582d43941cb19d0a0c619c7a7f319d3
Ecommerce FGR <marketplace@fgrsports.com.br>
2026-09-03 09:03:30 -0300

    feat: add autoprefixer and update Vite configuration for Tailwind CSS

--- vite.wc.config.ts
+++ vite.wc.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 const componentName = 'lp-ddc'
 
 export default defineConfig({
   plugins: [vue({ customElement: true })],
+
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
+
   build: {
     outDir: 'wc/' + componentName,
     emptyOutDir: true,
